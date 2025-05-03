fbc163f61d9ce730dda3f13879a49ba1f7faa313
RsMan-Dev <contact@rsman.fr>
2025-05-03 18:05:55 +0200

    avoid excessively deep types

--- src/proxy.d.ts
+++ src/proxy.d.ts
@@ -3,12 +3,12 @@ type ProxyTarget = Array<any> | Record<string | number, any> | Set<any> | Map<an
 type ProxyResult<T extends ProxyTarget, Hooks extends ProxyHandler<T>> = T & (Hooks extends { apply: (target: T, ...args: infer A) => infer R } ? { (...args: A): R } : {})
 
 interface ProxyHandler<T extends ProxyTarget> {
-  get?: (target: T, key: unknown, proxy: ProxyResult<T, this>) => any;
-  set?: (target: T, key: unknown, value: unknown, proxy: ProxyResult<T, this>) => boolean;
-  apply?: (target: T, proxy: ProxyResult<T, this>, ...args: unknown[]) => any;
-  ownKeys?: (target: T, proxy: ProxyResult<T, this>) => Array<keyof T>;
-  iter?: (target: T, proxy: ProxyResult<T, this>) => ReturnType<typeof pairs<T>>;
-  len?: (target: T, proxy: ProxyResult<T, this>) => number;
+  get?: (target: T, key: unknown, proxy: T) => any;
+  set?: (target: T, key: unknown, value: unknown, proxy: T) => boolean;
+  apply?: (target: T, proxy: T, ...args: unknown[]) => any;
+  ownKeys?: (target: T, proxy: T) => Array<keyof T>;
+  iter?: (target: T, proxy: T) => ReturnType<typeof pairs<T>>;
+  len?: (target: T, proxy: T) => number;
 }
 
 type ProxyConstructor = {
